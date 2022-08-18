9fddf978a4fd408f253df624eb3a8045d97d8607
TruwerM5 <ilsur2000.2016@gmail.com>
2022-08-18 22:50:22 +0300

    add changes

---
+++ assets/js/carousel.js
@@ -0,0 +1,7 @@
+let sliderItems = document.querySelectorAll('.carousel-item');
+let nextButton = document.querySelector('[data-carousel-next]');
+let prevButton = document.querySelector('[data-carousel-prev]');
+
+sliderItems[1].classList.add('scaled');
+
+console.log(sliderItems)
